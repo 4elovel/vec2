5a64c11b3a697cb14b2ae658b5c8fb550bc5d0c3
User <User@DESKTOP-FT49PKV>
2022-09-06 21:47:57 +0300

    Добавьте файлы проекта.

---
+++ vec2/vec2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int res = 0;
+	int j = 0;
+	srand(NULL);
+    vector <int> vec;
+	
+	for (size_t i = 0; i < 365; i++)
+	{
+		vec.push_back(rand() % 2);
+		cout << vec[i] << " ";
+	}
+	for (auto i = vec.begin(); i < vec.end(); i++)
+	{
+		if (*i == 1) {
+			res = j;
+			break;
+		}
+		j++;
+	}
+	cout << endl << endl << "First element with value 1 have an serial number " << res << endl;
+}
+
